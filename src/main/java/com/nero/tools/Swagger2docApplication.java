74dd97893e3a2a5669279301e7f5a154825cd311
nero <416739124@qq.com>
2018-10-19 15:18:24 +0800

    init

---
+++ src/main/java/com/nero/tools/Swagger2docApplication.java
@@ -0,0 +1,12 @@
+package com.nero.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2docApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2docApplication.class, args);
+	}
+}
